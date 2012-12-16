f2c647fe73f3e05048d6f05985a1e77455fdf494
Aleix Casanovas <al.casanovas.m@gmail.com>
2012-12-16 20:11:18 +0100

    Creació del MAIN
    Creem el main.m per agrupar l'entranament i l'aplicació de diferents
    llindars, i així no tenir un codi molt llarg i junt. Dividim el programa
    en diferents parts(funció)

--- train.m
+++ train.m
@@ -1,4 +1,4 @@
-
+function [mitjana_verd_natura,mitjana_verd_ciutat,mitjana_linia_natura,mitjana_linia_ciutat] = train
 numgrups = 0;
 
 dades_tots_grups = zeros(10,6,2);
@@ -232,32 +232,4 @@ end
   %Ara ja sabem el llindar perfecte, mitjançant les imatges
   %d'aprenantatge.. Per tant, utilitzarem aquests llindars per classificar
   %les demes imatges.
-  
-K_verd = (mitjana_verd_natura + mitjana_verd_ciutat)/2;
-K_linies = (mitjana_linia_natura + mitjana_linia_ciutat)/2;
-
-
-disp('CLASSIFICACIÓ MITJANÇANT EL LLINDAR APRÈS');
-grup = input('Quin grup imatges vols classificar? ','s');
-disp('');
-
-classificador(grup,K_verd,K_linies);
-
-%precissio i record()
-matrius(grup);
-
-disp('CLASSIFICACIÓ MITJANÇANT LLINDARS MANUALS');
-disp('Desitja posar uns llindars diferents?');
-sortir = 'n';
-while(sortir~='s')
-    
-K2_verd = input('Llindar de tant per cent de verd: ');
-K2_linies = input('Llindar del nombre de linies: ');
-
-classificador(grup,K2_verd,K2_linies);
-%precissio i record()
-matrius(grup);
-
-sortir = input('Desitja sortir?(s --> si | n --> no) ','s');
-end
         
