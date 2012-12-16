b8254e534f365c652b9f6131024a4424b125661b
Aleix Casanovas <al.casanovas.m@gmail.com>
2012-12-16 18:44:52 +0100

    Actualitzacio mapa2D i classificador
    El classificador ara actua com una funció en què s'ha li posa els
    diferents llindars desitjats. S'executa a mapa2D amb les mitjanes
    localitzades

--- classificador.m
+++ classificador.m
@@ -1,49 +1,59 @@
 %% CLASSIFICADOR DE IMATGES NATURE/CITY
 %  Aleix Casanovas, Sergi Alonso, Marc Andrés i Genís Matutes
 
-function classificador(file)
+function classificador(grup,llindar_verd,llindar_linies)
 
-%% GUARDA LES IMATGES
-num = 10;
-fitxer = fopen([file '.txt']);
+cont = zeros(1,10);
+global num;
+num = input('Quantes imatges vols classificar? ');
 
 for i = 0:(num-1)
-    fila = fgetl(fitxer);
-    nom_imatge = fila(1:4);
-    imatge = imread(nom_imatge,'jpg');
-    I(:,:,:,(i+1)) = imatge;
+    arxiu = strcat(grup,'-',num2str(i));
+    
+    im = imread(arxiu,'jpg');
+    I(:,:,:,(i+1)) = im;
+
+end
+s = size(I);
+
+%% CÀLCUL DELS VALORS RGB DE LA IMATGE
+
+for i = 1:num
+
+RGB_r(:,:,i) = I(:,:,1,i);
+RGB_g(:,:,i) = I(:,:,2,i);       
+RGB_b(:,:,i) = I(:,:,3,i);
+
 end
 
-%% CÀLCUL DE VERD
-% Guarda els nivells de verd de les imatges en un vector.
 
-% El vector nivells_verd conté el nivell de verd de les imatges
-% consecutives.
-nivells_verd = zeros(1,num);
+
+
+%% CÀLCUL DE VERD
+
+ver = zeros(1,num);
 probabilitat = zeros(1,num);
 
 for i = 1:num
     
     [green,prob] = verd(I(:,:,:,i));
-    nivells_verd(i) = green;
+    ver(i) = green;
     probabilitat(i) = prob;
     
-    if(probabilitat(i)>0.35)
+    if(probabilitat(i)>llindar_verd)
     cont(i) = cont(i) + 1;
-    disp(strcat(grup,'-',num2str(i-1),': Alt contingut de verd'));
-    disp('');
+    %disp(strcat(grup,'-',num2str(i-1),': Alt contingut de verd'));
+    %disp('');
 else
     cont(i) = cont(i) - 1;
-    disp(strcat(grup,'-',num2str(i-1),': Baix contingut de verd'));
-    disp('');
+    %disp(strcat(grup,'-',num2str(i-1),': Baix contingut de verd'));
+    %disp('');
     end
 end
 
 %% DETECCIÓ DE CONTORNS
-% TRANSFORMADA DE HOUGH
+%TRANSFORMADA DE HOUGH
 
-% Vectors on es guarda el nombre de línies horitzontal i vertical de 
-% cada imatge.
 numhoritzontal = zeros(1,10);
 numvertical = zeros(1,10);
 
@@ -51,33 +61,33 @@ for i = 1:num
     [numhor,numver] = detect(I(:,:,:,i));
     numhoritzontal(i) = numhor;
     numvertical(i) = numver;
-    disp(strcat(grup,'-',num2str(i-1),' Lines horitzontals: ',num2str(numhor)));
-    disp(strcat(grup,'-',num2str(i-1),' Lines verticals: ',num2str(numver)));
+    %disp(strcat(grup,'-',num2str(i-1),' Lines horitzontals: ',num2str(numhor)));
+    %disp(strcat(grup,'-',num2str(i-1),' Lines verticals: ',num2str(numver)));
     
-    if((numhor+numver) > 5)
-        cont(i) = cont(i) - 2;        
-    elseif((numhor+numver) <= 5)
-        cont(i) = cont(i) + 2;        
-    end    
+    if((numhor+numver) > llindar_linies)
+        cont(i) = cont(i) - 2;
+        
+    elseif((numhor+numver) <= llindar_linies)
+        cont(i) = cont(i) + 2;
+        
+    end
+    
 end
 
 
 %% EVALUACIÓ DELS RESULTATS
-% Extreure en una funció que ha de:
-%        - Escriure fitxer de sortida
-%        - Retornar el nom del fitxer de sortida perquè 
-%          el pugui llegir l'evaluador
 
-fid = fopen(strcat(file,'out.txt'),'w');
+fid=fopen(strcat(grup,'out.txt'),'w');
 
 for i = 1:num
     if(cont(i) < 0)
-         disp(strcat(grup,'-',num2str(i-1),' 0'));
+         %disp(strcat(grup,'-',num2str(i-1),' 0'));
          fprintf(fid, strcat(grup,'-',num2str(i-1),' 0'));
          fprintf(fid,'\n');
     elseif(cont(i) > 0)
-        disp(strcat(grup,'-',num2str(i-1),' 1'));
+        %disp(strcat(grup,'-',num2str(i-1),' 1'));
         fprintf(fid, strcat(grup,'-',num2str(i-1),' 1'));
         fprintf(fid,'\n');
     end
 end
+
