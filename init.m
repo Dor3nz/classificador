1af3ea2cb16d1025ebe28325171bd1598be018eb
Dor3nz <genis.matutes@gmail.com>
2012-12-18 15:37:09 +0100

    Actualitza llindars manuals per acumulacio

--- init.m
+++ init.m
@@ -60,6 +60,14 @@ end
 n_imatges = mat2cell(n_imatges);
 set(handles.n_imatges_popup, 'String', n_imatges);
 
+% Assignació de variables globals per l'acumulació de valors de precisió i
+% record.
+global x_record_plot;
+global y_precisio_plot;
+
+x_record_plot = 0;
+y_precisio_plot = 1;
+
 
 % --- Outputs from this function are returned to the command line.
 function varargout = init_OutputFcn(hObject, eventdata, handles) 
@@ -331,39 +339,27 @@ function itera_endavant_Callback(hObject, eventdata, handles)
 function push_classifica_manual_Callback(hObject, eventdata, handles)
 global grup;
 global num;
+global x_record_plot;
+global y_precisio_plot;
 
 % Canvia status a classificant.
 set(handles.text_estat, 'String', 'Classificant...');
 drawnow
 
 % Lectura dels llindars entrats per l'usuari.
-llindar_verd = str2num(get(handles.manual_verd, 'String'));
+llindar_verd = (str2num(get(handles.manual_verd, 'String')) / 100);
 llindar_linies = str2num(get(handles.manual_linies, 'String'));
 
 % Càlcul de precisió i record.
 [x_record, y_precisio] = classificacio_llindars_manuals(grup, num, llindar_verd, llindar_linies);
 
 % Dibuix de la gràfica.
-cla(handles.axes_prec_rec);
-x_record = [0 x_record];
-y_precisio = [1 y_precisio];
 
-size_record = size(x_record);
-size_precisio = size(y_precisio);
+x_record_plot = [x_record_plot x_record];
+y_precisio_plot = [y_precisio_plot y_precisio];
 
-x_record_plot = zeros(1,size_record(2));
-y_precisio_plot = zeros(1,size_precisio(2));
-
-for q = 1:size_record(2)
-    [x_record_plot(q),posicio] = min(x_record);
-    x_record(posicio) = 2;
-    y_precisio_plot(q) = y_precisio(posicio);    
-end
-
-size_r_plot = size(x_record_plot);
-
-x_record_plot = [x_record_plot x_record_plot(size_r_plot(2))];
-y_precisio_plot = [y_precisio 0];
+[x_record_plot, record_order] = sort(x_record_plot);
+y_precisio_plot = y_precisio_plot(record_order);
 
 plot(handles.axes_prec_rec, x_record_plot,y_precisio_plot,'-rs',...
                 'MarkerEdgeColor','k',...
